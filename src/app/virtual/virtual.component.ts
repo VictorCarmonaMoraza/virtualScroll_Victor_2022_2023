610ba3fe499f181a4d235bcd7859af973efdbc20
Victor <VictorCarmona9035@hotmail.com>
2022-11-07 20:04:24 +0100

    2-Virtual Scroll

---
+++ src/app/virtual/virtual.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-virtual',
+  templateUrl: './virtual.component.html',
+  styleUrls: ['./virtual.component.css']
+})
+export class VirtualComponent implements OnInit {
+
+//Creamos y rellenamos un arreglo de ceros de 500 posiciones
+  personas = Array(500).fill(0);
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log(this.personas);
+  }
+
+}
