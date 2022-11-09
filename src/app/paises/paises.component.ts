070735b7b98b8f64a38065e1229c3a6bf147ba18
Victor <VictorCarmona9035@hotmail.com>
2022-11-09 21:07:16 +0100

    6-Listado con colores

--- src/app/paises/paises.component.ts
+++ src/app/paises/paises.component.ts
@@ -24,7 +24,37 @@ export class PaisesComponent implements OnInit {
 
   drop(evento: CdkDragDrop<any>) {
     console.log("OK", evento);
-    moveItemInArray(this.paises,evento.previousIndex,evento.currentIndex);
+    moveItemInArray(this.paises, evento.previousIndex, evento.currentIndex);
+  }
+
+  getColorCard(d: number) {
+    let color: string;
+    // switch (d) {
+    //   case 0:
+    //     return color = '#000000';
+    //   case 1:
+    //     return color = '#1e98e4';
+    //   case 2:
+    //     return color = '#ff2a00';
+    //   case 3:
+    //     return color = '#008000';
+    // }
+    color=this.colorHEX();
+    return color;
+  }
+
+   generarLetra(){
+    var letras = ["a","b","c","d","e","f","0","1","2","3","4","5","6","7","8","9"];
+    var numero = (Math.random()*15).toFixed(0);
+    return letras[numero];
+  }
+
+   colorHEX(){
+    var coolor = "";
+    for(var i=0;i<6;i++){
+      coolor = coolor + this.generarLetra() ;
+    }
+    return "#" + coolor;
   }
 
 }
