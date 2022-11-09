8c469720f25864c2565f7897f0719f3ee37b0d6b
Victor <VictorCarmona9035@hotmail.com>
2022-11-09 18:55:07 +0100

    5-Drag & Drop de una lista ordenada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,21 @@ import { ScrollingModule } from '@angular/cdk/scrolling';
 import 'hammerjs';
 import { DragComponent } from './drag/drag.component';
 import {DragDropModule} from '@angular/cdk/drag-drop';
+import { PaisesComponent } from './paises/paises.component';
+import{HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     VirtualComponent,
-    DragComponent
+    DragComponent,
+    PaisesComponent
   ],
   imports: [
     BrowserModule,
     ScrollingModule,
-    DragDropModule
+    DragDropModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
